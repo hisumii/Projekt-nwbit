e7a375ce3c97cc9da884410f1d8ce8e11127029b
Martyna Oleszczuk <207635872+hisumii@users.noreply.github.com>
2025-06-12 01:43:14 +0200

    add: yaml load

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import argparse
 import traceback
 import json
+import yaml
 
 parser = argparse.ArgumentParser(description="Konwerter plików JSON/XML/YAML")
 parser.add_argument('input_file', help='Plik wejściowy')
 parser.add_argument('output_file', help='Plik wyjściowy')
 args = parser.parse_args()
 
-with open(args.input_file, 'r') as f:
-    try:
-        data = json.load(f)
-    except Exception as e:
-        print("Nie udało się wczytać pliku JSON. ", traceback.format_exc())
+if args.input_file.endswith('.json'):
+    with open(args.input_file, 'r') as f:
+        try:
+            data = json.load(f)
+        except Exception as e:
+            print("Nie udało się wczytać pliku JSON. ", traceback.format_exc())
 
-with open(args.output_file, 'w') as f:
-    try:
-        json.dump(data, f, indent=2)
-    except Exception as e:
-        print("Nie udało się zapisać pliku JSON. ", traceback.format_exc())
+    with open(args.output_file, 'w') as f:
+        try:
+            json.dump(data, f, indent=2)
+        except Exception as e:
+            print("Nie udało się zapisać pliku JSON. ", traceback.format_exc())
+elif args.input_file.endswith('.yml') or args.input_file.endswith('.yaml'):
+    with open(args.input_file, 'r') as f:
+        try:
+            data = yaml.safe_load(f)
+        except Exception as e:
+            print("Nie udało się wczytać pliku YAML. ", traceback.format_exc())
+
+elif args.input_file.endswith('.xml'):
+    # wczytaj XML
+else:
+    print("Nieznany format pliku!")
+
+
