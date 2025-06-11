b3986f4388b8c8f29eadc58291e88269f2755cc5
Martyna Oleszczuk <207635872+hisumii@users.noreply.github.com>
2025-06-12 01:37:23 +0200

    add: json load

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import argparse
+import traceback
+import json
 
 parser = argparse.ArgumentParser(description="Konwerter plików JSON/XML/YAML")
 parser.add_argument('input_file', help='Plik wejściowy')
 parser.add_argument('output_file', help='Plik wyjściowy')
 args = parser.parse_args()
+
+with open(args.input_file, 'r') as f:
+    try:
+        data = json.load(f)
+    except Exception as e:
+        print("Nie udało się wczytać pliku JSON. ", traceback.format_exc())
